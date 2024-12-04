6698da3de5b2000ca2d828d8e573643260fffc76
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-04 23:25:47 +0600

    showing highest rated games data in UI

--- src/components/HomePage/HighRatedGamesCard/HighRatedGamesCard.jsx
+++ src/components/HomePage/HighRatedGamesCard/HighRatedGamesCard.jsx
@@ -1,12 +1,24 @@
+const HighRatedGamesCard = ({ game }) => {
+  const { _id, name, rating, description, image } = game;
 
-
-const HighRatedGamesCard = ({game}) => {
-    
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div class="card bg-base-100 w-[420px] h-[600px] border-2 border-slate-300">
+        <figure>
+          <img src={image} />
+        </figure>
+        <div class="card-body">
+          <h2 class="text-3xl font-extrabold">{name}</h2>
+          <p>{rating}</p>
+          <div class="card-actions ">
+            <button class="border rounded-xl p-2 text-blue-700 bg-blue-100 border-blue-300 w-full mt-4 font-bold shadow-md hover:shadow-[0px_10px_30px_rgba(0,0,0,0.3)] transition-shadow duration-300 ease-in-out text-base">
+              Explore Details
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default HighRatedGamesCard;
--- src/components/HomePage/HighestRatedGames/HighestRatedGames.jsx
+++ src/components/HomePage/HighestRatedGames/HighestRatedGames.jsx
@@ -5,12 +5,12 @@ const HighestRatedGames = () => {
   const highratedGames = useLoaderData();
 
   return (
-    <div className="font-1 mt-16 mb-72">
+    <div className="font-1 mt-36 mb-72">
       <h1 className="text-5xl font-extrabold text-center">
         Highest Rated Games {highratedGames.length}{" "}
       </h1>
 
-      <div>
+      <div className="grid grid-cols-3 ml-10 mt-16 gap-y-16">
         {highratedGames.map((game) => (
           <HighRatedGamesCard key={game._id} game={game}></HighRatedGamesCard>
         ))}
