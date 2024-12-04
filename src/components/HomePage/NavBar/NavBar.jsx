1645a171216977f45b25709e32e38734fd219ad8
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-04 19:27:13 +0600

    added dropdown menu in navbar

--- src/components/HomePage/NavBar/NavBar.jsx
+++ src/components/HomePage/NavBar/NavBar.jsx
@@ -1,7 +1,6 @@
 import logo from "../../../assets/images/logo/websiteLogo.jpg";
 import { Link, NavLink } from "react-router-dom";
-
-import icon4 from "../../../assets/images/icons/icon4.png";
+import { FaUserCircle } from "react-icons/fa";
 import "./NavBar.css";
 
 const NavBar = () => {
@@ -23,7 +22,24 @@ const NavBar = () => {
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <div className="dropdown dropdown-bottom dropdown-end dropdown-hover">
+            <div tabIndex={0} role="button">
+              <h1 className="text-5xl">
+                <FaUserCircle />
+              </h1>
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
