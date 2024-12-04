5b2ec7a4445f0e2edd30ad604bee500839323fb1
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-04 18:10:55 +0600

    added website logo in navbar

--- src/layout/MainLayout/MainLayout.jsx
+++ src/layout/MainLayout/MainLayout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
+import NavBar from "../../components/HomePage/NavBar/NavBar";
 
 const MainLayout = () => {
   return (
-    <div>
-      <h1>Heloo Game</h1>
+    <div className="border-2 border-red-600  mt-8 max-w-[1500px] h-80 mx-auto">
+      <NavBar></NavBar>
       <Outlet></Outlet>
     </div>
   );
