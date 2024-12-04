0cb9cd085c66c9a0467c43e38955d21629708eda
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-04 23:46:54 +0600

    added dynamic rating in highest rated games card

--- src/components/HomePage/HighRatedGamesCard/HighRatedGamesCard.jsx
+++ src/components/HomePage/HighRatedGamesCard/HighRatedGamesCard.jsx
@@ -1,5 +1,15 @@
+import { Heart, Rating, ThinStar } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+
 const HighRatedGamesCard = ({ game }) => {
   const { _id, name, rating, description, image } = game;
+
+  const myStyles = {
+    itemShapes: Heart,
+    activeFillColor: "#FF007F",
+    inactiveFillColor: "#FFC0CB",
+  };
 
   return (
     <div>
@@ -9,7 +19,15 @@ const HighRatedGamesCard = ({ game }) => {
         </figure>
         <div class="card-body">
           <h2 class="text-3xl font-extrabold">{name}</h2>
-          <p>{rating}</p>
+          <div className="flex items-center gap-4 mt-6">
+            <Rating
+              style={{ maxWidth: 180 }}
+              itemStyles={myStyles}
+              value={rating}
+              readOnly
+            />
+            <p>{rating}</p>
+          </div>
           <div class="card-actions ">
             <button class="border rounded-xl p-2 text-blue-700 bg-blue-100 border-blue-300 w-full mt-4 font-bold shadow-md hover:shadow-[0px_10px_30px_rgba(0,0,0,0.3)] transition-shadow duration-300 ease-in-out text-base">
               Explore Details
