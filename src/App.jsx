c6320cb53170993624df85178221779e13a60a27
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-04 11:24:39 +0600

    added google fonts

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ function App() {
 
   return (
     <>
-     
-      <h1>GameLoom</h1>
-      
+      <h1 className="mt-12 text-center font-1">GameLoom1</h1>
+      <h1 className="mt-12 text-center font-2">GameLoom2</h1>
+      <h1 className="mt-12 text-center font-3">GameLoom3</h1>
+      <h1 className="mt-12 text-center font-4">GameLoom4</h1>
+      <h1 className="mt-12 text-center font-5">GameLoom5</h1>
     </>
-  )
+  );
 }
 
 export default App
