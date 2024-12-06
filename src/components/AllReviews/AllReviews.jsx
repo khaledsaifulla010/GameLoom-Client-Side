0ea7920f7667e85b8f3b953bd54c0d5122fe1cb7
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-06 13:57:42 +0600

    added explore details button in review details card

--- src/components/AllReviews/AllReviews.jsx
+++ src/components/AllReviews/AllReviews.jsx
@@ -12,7 +12,7 @@ const AllReviews = () => {
         <h1 className="mt-4 text-center text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-teal-600">
           All Reviews
         </h1>
-        <div className="grid grid-cols-1 ml-10 mt-16 gap-y-16 ">
+        <div className="grid grid-cols-1 ml-4 mt-16 gap-y-16 ">
             {
                 reviews.map(review => <AllReviewsCard key={review._id} review={review}></AllReviewsCard>)
             }
--- src/components/AllReviewsCard/AllReviewsCard.jsx
+++ src/components/AllReviewsCard/AllReviewsCard.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 const AllReviewsCard = ({ review }) => {
-  const { name, gameName, gameThumbnail, genreSelect } = review;
+  const { _id, name, gameName, gameThumbnail, genreSelect } = review;
 
   return (
     <div>
-      <div className="flex justify-center items-center">
-        <div className="card bg-base-100 border-2 border-slate-300 w-[950px] h-[300px] overflow-hidden shadow-md transition-transform duration-1000 ease-in-out transform hover:shadow-[0px_15px_35px_rgba(0,0,0,0.5)] hover:scale-105">
+      <div className="flex  ">
+        <div className="card bg-slate-100 border-2 border-slate-300 w-[950px] h-[300px] overflow-hidden shadow-md transition-transform duration-1000 ease-in-out transform hover:shadow-[0px_15px_35px_rgba(0,0,0,0.5)] hover:scale-105">
           <figure className="flex h-full">
             <img
               src={gameThumbnail}
@@ -15,18 +17,26 @@ const AllReviewsCard = ({ review }) => {
               <h3 className="text-2xl font-black text-indigo-800 border w-full text-center p-2 rounded-xl border-indigo-200 bg-indigo-50 ">
                 {gameName}
               </h3>
-              <div className="divider mt-12"></div>
-              <div className="flex items-center justify-between mt-4">
-                <div className="mt-4 text-xl text-gray-600 ">
-                  <p className="font-semibold text-teal-700 border p-1 w-full rounded-xl bg-teal-50 border-teal-200 text-center">
+              <div className="divider mt-8"></div>
+              <div className="flex items-center justify-between mt-2">
+                <div className="mt-2 text-xl text-gray-600 ">
+                  <p className="font-semibold text-teal-700 border px-2 w-full rounded-xl bg-teal-50 border-teal-200 text-center">
                     <strong>Reviewer :</strong> {name}
                   </p>
                 </div>
-                <div className="mt-4 text-xl text-gray-600 ">
-                  <p className="font-semibold text-rose-700 border p-1 w-full rounded-xl bg-rose-50 border-rose-200 text-center italic">
+                <div className="mt-2 text-xl text-gray-600 ">
+                  <p className="font-semibold text-rose-700 border px-2 w-full rounded-xl bg-rose-50 border-rose-200 text-center italic">
                     <strong>Genre :</strong> {genreSelect}
                   </p>
                 </div>
+              </div>
+              <div class="card-actions ">
+                <Link
+                  to={`/reviews/${_id}`}
+                  class="border rounded-xl p-2 text-blue-700 bg-blue-100 border-blue-300 w-full mt-8 font-bold shadow-md hover:shadow-[0px_10px_30px_rgba(0,0,0,0.3)] transition-shadow duration-300 ease-in-out text-base text-center "
+                >
+                  Explore Details
+                </Link>
               </div>
             </div>
           </figure>
