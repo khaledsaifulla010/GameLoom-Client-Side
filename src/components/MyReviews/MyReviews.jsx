6326307bc4268d8f7acd03f67187d3dbed82b818
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-07 15:14:31 +0600

    update my review using modals

--- src/components/MyReviews/MyReviews.jsx
+++ src/components/MyReviews/MyReviews.jsx
@@ -1,18 +1,70 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBin2Fill } from "react-icons/ri";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const MyReviews = () => {
-  const { userEmail } = useContext(AuthContext);
+  const { userEmail, user } = useContext(AuthContext);
 
   const myReviews = useLoaderData();
+  const [reviews, setReviews] = useState(myReviews);
 
-  const filteredReviews = myReviews.filter(
+  const filteredReviews = reviews.filter(
     (review) => review.userEmail === userEmail
   );
 
-  console.log(filteredReviews);
+  const handleUpdateReview = (e, _id) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const gameName = e.target.gameName.value;
+    const genreSelect = e.target.genreSelect.value;
+    const publicationYear = e.target.publicationYear.value;
+    const email = e.target.email.value;
+    const reviewDescription = e.target.reviewDescription.value;
+    const rating = e.target.rating.value;
+    const gameThumbnail = e.target.gameThumbnail.value;
+
+    const newReview = {
+      name,
+      gameName,
+      genreSelect,
+      publicationYear,
+      email,
+      reviewDescription,
+      rating,
+      gameThumbnail,
+    };
+    const updateReview = { ...newReview, userEmail };
+
+    fetch(`http://localhost:5000/reviews/${_id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateReview),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          toast.success("Review updated successfully!", {
+            position: "top-center",
+          });
+          setReviews((prevReviews) =>
+            prevReviews.map((review) =>
+              review._id === _id ? { ...review, ...newReview } : review
+            )
+          );
+          document.getElementById(`modal-${_id}`).close();
+        } else {
+          toast.error("Failed to update the review.", {
+            position: "top-center",
+          });
+        }
+      });
+  };
 
   return (
     <div className="mb-24 font-1">
@@ -79,25 +131,182 @@ const MyReviews = () => {
                           {game.publicationYear}
                         </span>
                       </td>
-                      <td className="text-center py-3 px-4 font-bold text-blue-600 text-2xl">
+                      <td className="text-center py-3 px-4 font-bold  text-2xl">
                         <button
                           className="ml-4"
                           onClick={() =>
-                            document.getElementById("my_modal_4").showModal()
+                            document
+                              .getElementById(`modal-${game._id}`)
+                              .showModal()
                           }
                         >
-                          <FaEdit />
+                          <FaEdit className="text-blue-600" />
                         </button>
-                        <dialog id="my_modal_4" className="modal">
-                          <div className="modal-box w-11/12 max-w-5xl">
-                            <h3 className="font-bold text-lg">Hello!</h3>
-                            <p className="py-4">
-                              Click the button below to close
-                            </p>
-                            <div className="modal-action">
-                              <form method="dialog">
-                                {/* if there is a button, it will close the modal */}
-                                <button className="btn">Close</button>
+                        <dialog id={`modal-${game._id}`} className="modal">
+                          <div className="modal-box max-w-[1200px]">
+                            <form method="dialog">
+                              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+                                ✕
+                              </button>
+                            </form>
+                            <div>
+                              <form
+                                onSubmit={(e) =>
+                                  handleUpdateReview(e, game._id)
+                                }
+                                className="flex items-center justify-center mt-12"
+                              >
+                                <div className="border border-slate-300 w-[1000px] p-4 rounded-xl h-[520px] ">
+                                  <div className="flex items-center justify-center gap-8">
+                                    {/* First 4 Properties */}
+                                    <div className="w-[450px] space-y-2">
+                                      {/* Name */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Name
+                                          </span>
+                                        </label>
+                                        <input
+                                          type="text"
+                                          name="name"
+                                          defaultValue={user.displayName}
+                                          readOnly
+                                          className="input input-bordered shadow-md transition duration-300 ease-in-out hover:shadow-lg text-slate-400 font-semibold"
+                                          required
+                                        />
+                                      </div>
+                                      {/* Game Name */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Game Name
+                                          </span>
+                                        </label>
+                                        <input
+                                          type="text"
+                                          name="gameName"
+                                          defaultValue={game.gameName}
+                                          className="input input-bordered shadow-md transition duration-300 ease-in-out hover:shadow-lg"
+                                          required
+                                        />
+                                      </div>
+                                      {/* Genres */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Genres
+                                          </span>
+                                        </label>
+                                        <select
+                                          className="select select-bordered w-full shadow-md transition duration-300 ease-in-out hover:shadow-lg"
+                                          name="genreSelect"
+                                          defaultValue={game.genreSelect}
+                                        >
+                                          <option disabled>
+                                            Enter Games Genre
+                                          </option>
+                                          <option>Action</option>
+                                          <option>RPG</option>
+                                          <option>Sandbox</option>
+                                          <option>Adventure</option>
+                                          <option>Strategy</option>
+                                        </select>
+                                      </div>
+                                      {/* Publication Year */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Publication Year
+                                          </span>
+                                        </label>
+                                        <input
+                                          type="number"
+                                          name="publicationYear"
+                                          defaultValue={game.publicationYear}
+                                          className="input input-bordered shadow-md transition duration-300 ease-in-out hover:shadow-lg"
+                                          min="1980"
+                                          max={new Date().getFullYear()}
+                                          placeholder="Enter Publication Year"
+                                          required
+                                        />
+                                      </div>
+                                    </div>
+                                    {/* Second 4 Properties */}
+                                    <div className="w-[450px] space-y-2">
+                                      {/* Email */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Email
+                                          </span>
+                                        </label>
+                                        <input
+                                          type="email"
+                                          name="email"
+                                          defaultValue={user.email}
+                                          readOnly
+                                          className="input input-bordered shadow-md transition duration-300 ease-in-out hover:shadow-lg text-slate-400 font-semibold"
+                                          required
+                                        />
+                                      </div>
+                                      {/* Review Description */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Review Description
+                                          </span>
+                                        </label>
+                                        <input
+                                          type="text"
+                                          name="reviewDescription"
+                                          defaultValue={game.reviewDescription}
+                                          className="input input-bordered shadow-md transition duration-300 ease-in-out hover:shadow-lg"
+                                          required
+                                        />
+                                      </div>
+                                      {/* Rating */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Rating
+                                          </span>
+                                        </label>
+                                        <input
+                                          type="number"
+                                          name="rating"
+                                          defaultValue={game.rating}
+                                          min={1.0}
+                                          max={5.0}
+                                          step={0.1}
+                                          className="input input-bordered shadow-md transition duration-300 ease-in-out hover:shadow-lg"
+                                          required
+                                        />
+                                      </div>
+                                      {/* Thumbnail */}
+                                      <div className="form-control">
+                                        <label className="label">
+                                          <span className="label-text text-lg font-bold">
+                                            Game Thumbnail
+                                          </span>
+                                        </label>
+                                        <input
+                                          type="url"
+                                          name="gameThumbnail"
+                                          defaultValue={game.gameThumbnail}
+                                          className="input input-bordered shadow-md transition duration-300 ease-in-out hover:shadow-lg"
+                                          required
+                                        />
+                                      </div>
+                                    </div>
+                                  </div>
+
+                                  <div className="mt-8">
+                                    <button className="border p-2 ml-4 w-[930px] rounded-xl text-pink-600 bg-pink-100 border-pink-300 font-extrabold text-xl shadow-md transition duration-500 ease-in-out transform hover:scale-105 active:scale-95">
+                                      Submit
+                                    </button>
+                                  </div>
+                                </div>
                               </form>
                             </div>
                           </div>
