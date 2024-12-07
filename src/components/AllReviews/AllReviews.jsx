c957ee06f84beeac6287f7c1d0ea025020816d66
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-07 18:46:49 +0600

    added sort by rating and rating icons

--- src/components/AllReviews/AllReviews.jsx
+++ src/components/AllReviews/AllReviews.jsx
@@ -2,6 +2,8 @@ import { useLoaderData } from "react-router-dom";
 import AllReviewsCard from "../AllReviewsCard/AllReviewsCard";
 import { useContext } from "react";
 import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
+import { ImSortAmountDesc } from "react-icons/im";
+import { FaStar } from "react-icons/fa";
 
 const AllReviews = () => {
   const reviews = useLoaderData();
@@ -17,7 +19,7 @@ const AllReviews = () => {
 
   return (
     <div className="mt-12 mb-24 font-4">
-      <div className="flex items-center justify-between mt-16 mb-24">
+      <div className="flex items-center justify-around mt-16 mb-24">
         <div className="border w-[400px] p-2 text-center text-lg font-semibold text-purple-700 border-purple-200 bg-purple-50 rounded-xl">
           <h1>Name : {user.displayName} </h1>
           <h1>Email : {user.email} </h1>
@@ -26,6 +28,27 @@ const AllReviews = () => {
           <h1 className=" text-center text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-teal-600">
             All Reviews
           </h1>
+        </div>
+        <div>
+          <div className="dropdown dropdown-bottom">
+            <div
+              tabIndex={0}
+              role="button"
+              className="flex items-center gap-1 font-bold text-xl text-orange-600 border p-2 rounded-xl bg-orange-50 border-orange-100"
+            >
+              Sort By <ImSortAmountDesc className="mt-1" />
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+              <li className=" font-semibold text-rose-600 border rounded-xl bg-rose-50 border-rose-100">
+                <h1 className="ml-12">
+                  Rating <FaStar />
+                </h1>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
