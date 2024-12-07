554613c20f126a29d8452ffe6ce69851fd14c238
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-07 19:20:21 +0600

    added sort functionality for rating & year in review

--- src/components/AllReviews/AllReviews.jsx
+++ src/components/AllReviews/AllReviews.jsx
@@ -1,6 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import AllReviewsCard from "../AllReviewsCard/AllReviewsCard";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProvider/AuthProvider";
 import { ImSortAmountDesc } from "react-icons/im";
 import { FaStar, FaFilter, FaLightbulb } from "react-icons/fa";
@@ -10,6 +10,9 @@ const AllReviews = () => {
   const reviews = useLoaderData();
   const { user, loading } = useContext(AuthContext);
 
+  const [sortOption, setSortOption] = useState(null); // Set initial state to null (no sorting)
+  const [sortedReviews, setSortedReviews] = useState(reviews); // State to hold sorted reviews
+
   if (loading) {
     return <span className="loading loading-infinity loading-xs"></span>;
   }
@@ -18,9 +21,25 @@ const AllReviews = () => {
     return <p className="text-center text-red-500">User not logged in.</p>;
   }
 
+  // HANDLE SORTING USNIG RATING & YEAR
+
+  const handleSort = (option) => {
+    setSortOption(option);
+
+    let sorted = [...reviews];
+
+    if (option === "rating") {
+      sorted = sorted.sort((a, b) => b.rating - a.rating);
+    } else if (option === "year") {
+      sorted = sorted.sort((a, b) => b.publication_year - a.publication_year);
+    }
+
+    setSortedReviews(sorted);
+  };
+
   return (
     <div className="mt-12 mb-24 font-4">
-      <div className="flex items-center justify-around mt-16 mb-28">
+      <div className="flex items-center justify-around gap-36 mt-16 mb-28">
         <div className="border w-[400px] p-2 text-center text-lg font-semibold text-purple-700 border-purple-200 bg-purple-50 rounded-xl">
           <h1>Name : {user.displayName} </h1>
           <h1>Email : {user.email} </h1>
@@ -44,12 +63,18 @@ const AllReviews = () => {
                 tabIndex={0}
                 className="dropdown-content menu bg-base-100 rounded-box z-[1] w-28 p-2 shadow gap-y-4 border-2"
               >
-                <li className=" font-semibold text-rose-600 border rounded-xl bg-rose-50 border-rose-100 w-24">
+                <li
+                  className="font-semibold text-rose-600 border rounded-xl bg-rose-50 border-rose-100 w-24"
+                  onClick={() => handleSort("rating")}
+                >
                   <h1>
                     Rating <FaStar />
                   </h1>
                 </li>
-                <li className=" font-semibold text-blue-600 border rounded-xl bg-blue-50 border-blue-100 w-24">
+                <li
+                  className="font-semibold text-blue-600 border rounded-xl bg-blue-50 border-blue-100 w-24"
+                  onClick={() => handleSort("year")}
+                >
                   <h1>
                     Year <PiCalendarStarFill />
                   </h1>
@@ -68,7 +93,7 @@ const AllReviews = () => {
                 tabIndex={0}
                 className="dropdown-content menu bg-base-100 rounded-box z-[1] w-28 p-2 shadow gap-y-4 border-2"
               >
-                <li className=" font-semibold text-yellow-600 border rounded-xl bg-yellow-50 border-yellow-100 w-24">
+                <li className="font-semibold text-yellow-600 border rounded-xl bg-yellow-50 border-yellow-100 w-24">
                   <h1>
                     Genre <FaLightbulb />
                   </h1>
@@ -80,7 +105,7 @@ const AllReviews = () => {
       </div>
 
       <div className="grid grid-cols-2 ml-10 gap-x-4 mt-16 gap-y-16">
-        {reviews.map((review) => (
+        {sortedReviews.map((review) => (
           <AllReviewsCard key={review._id} review={review}></AllReviewsCard>
         ))}
       </div>
